7255a9c4e00a5adc37c1d3309464f97a785a974e
Vishnuprajapati <vishnu.prajapati@techcronus.com>
2023-04-20 18:52:30 +0530

    new learing

--- JavaScript from udemy/JS fundamentals part-1/starter/main.js
+++ JavaScript from udemy/JS fundamentals part-1/starter/main.js
@@ -53,7 +53,6 @@ const birthYear = 1996;
 
 /*
 // ============= Basic Operators ============
-*/
 const firstName = 'Maulik';
 const lastName = 'patel';
 const space = ' ';
@@ -67,3 +66,89 @@ console.log(maulikAge > akshayAge);
 console.log(maulikAge < akshayAge);
 console.log(maulikAge == akshayAge);
 console.log(swetaAge >= maulikAge);
+*/
+
+/*
+============ Operators Precedence ===============
+const now = 2023;
+const ageMaulik = now - 1996;
+const ageSweta = now - 1995;
+console.log(ageMaulik, ageSweta);
+
+const firstName = 'Maulik';
+const lastName = 'Patel';
+console.log(firstName, lastName);
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+*/
+
+/*
+// Coding Challenge #1
+
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+
+console.log('Codding Challenge #1');
+let weightMark, weightJohn, heightMark, heightJohn;
+// bmi = mass / height ** 2;
+weightMark = 78;
+weightJohn = 92;
+heightMark = 1.69;
+heightJohn = 1.95;
+let bmiMark = weightMark / heightMark ** 2;
+let bmiJohn = weightJohn / heightJohn ** 2;
+console.log('Mark bmi = ', bmiMark);
+console.log('John bmi = ', bmiJohn);
+
+console.log('comparing bmi : John Higher?', bmiJohn > bmiMark);
+*/
+
+/* 
+=========== String and Template literals ==========
+const firstName = 'maulik';
+const lastName = 'Patel';
+const age = 26;
+
+console.log("i'm " + firstName + ' ' + lastName + " and I'm " + age + 'year old');
+
+console.log(`I'm $(firstName)`);
+
+const Template = `i"m Maulik
+Patel
+and 
+well`;
+console.log(Template);
+*/
+
+/*
+============== if / else statement =============
+*/
+const age = 16;
+
+if (age >= 18) {
+  console.log('can start driving');
+} else {
+  console.log('wait until you are 18');
+}
+
+let century;
+const birthYear = 1996;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
