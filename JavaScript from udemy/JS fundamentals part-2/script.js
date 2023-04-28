d85a3a8e94c6a27e3003650ff8574049ee741f11
Vishnuprajapati <vishnu.prajapati@techcronus.com>
2023-04-28 18:10:03 +0530

    learn up to array

--- JavaScript from udemy/JS fundamentals part-2/script.js
+++ JavaScript from udemy/JS fundamentals part-2/script.js
@@ -92,7 +92,7 @@ const mixJuice = foodProcessor(5, 7);
 console.log(mixJuice);
 */
 
-/*============================== Reviewing function ===================================*/
+/*============================== Reviewing function ===================================
 const calAge3 = function (birthyear) {
   return 2023 - birthyear;
 };
@@ -110,3 +110,176 @@ console.log(retairYear(1996, 'Maulik'));
 console.log(retairYear(1995, 'Sweta'));
 console.log(retairYear(1970, 'Rajendra'));
 console.log(retairYear(1940, 'Sita'));
+*/
+
+/*================================ Codding Challenge #1 ===============================
+// Coding Challenge #1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+// Hints:
+// § To calculate average of 3 values, add them all together and divide by 3
+// § To check if number A is at least double number B, check for A >= 2 * B.
+// Apply this to the team's average scores �
+// GOOD LUCK �
+console.log(`challenge #1`);
+let dAvg = (70 + 112 + 101) / 3;
+let kAvg = (70 + 95 + 106) / 3;
+
+console.log(dAvg, kAvg);
+
+if (dAvg > kAvg && dAvg >= 100) {
+  console.log(`D wins!`);
+} else if (kAvg > dAvg && kAvg >= 100) {
+  console.log(`K wins!`);
+} else if (kAvg === dAvg && kAvg >= 100 && dAvg >= 100) {
+  console.log(`it is a draw!`);
+} else {
+  console.log(`both are looser`);
+}
+
+//using function
+const calAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDol = calAverage(85, 54, 41);
+const avgkoa = calAverage(23, 333, 32);
+
+const checkWinner = function () {
+  if (avgDol >= 2 * avgkoa) {
+    console.log(`Dol wins!`);
+  } else if (avgkoa >= 2 * avgDol) {
+    console.log(`koa wins!`);
+  } else {
+    console.log(`both are loosers!`);
+  }
+  return;
+};
+
+checkWinner();
+*/
+
+/*==================== Introduction to Arrays ====================
+const friends = ['Abhi', 'Ahemad', 'Zill'];
+console.log(friends);
+
+const years = new Array(1996, 1995, 1998);
+console.log(years);
+
+console.log(friends.length);
+console.log(friends[1]);
+console.log(years[3]);
+console.log(years[2]);
+console.log(friends[friends.length - 1]); //second last element
+
+friends[2] = 'Avas';
+console.log(friends[2]);
+
+const calAge = function (birthyear) {
+  return 2023 - birthyear;
+};
+
+const yearsOldSingle = calAge(years[0]);
+console.log(yearsOldSingle);
+
+const yearsOld = [calAge(years[0]), calAge(years[1])];
+console.log(yearsOld);
+*/
+
+/*=================== Basic Array Operations(Methods) ===========================
+const friends = ['Abhi', 'Ahemad', 'Zill'];
+// add element at the End
+friends.push('Prabhas');
+console.log(friends, friends.length);
+// add element at the Start of array
+friends.unshift('Pitter');
+console.log(friends, friends.length);
+// remove last element
+friends.pop();
+console.log(friends, friends.length);
+// remove First element
+friends.shift();
+console.log(friends, friends.length);
+//find index of value
+console.log(friends.indexOf('Abhi'));
+// check if checking element is present in array or not
+console.log(friends.includes('Zill'));
+if (friends.includes('axay')) {
+  console.log(`u have friend named Axay`);
+} else {
+  console.log(`u do not have friends name Axay`);
+}
+*/
+
+/*=================== Coding Challenge #2 ==========================
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) �
+// GOOD LUCK �
+
+// let tip = 0; // need to define because it will use to store value
+
+// const calcTip = function (bill) {
+//   const tipValue = bill >= 50 && bill <= 300 ? (tip = bill * 0.15) : (tip = bill * 0.2);
+//   console.log(tip);
+// };
+
+// calcTip(100);
+
+// const bills = [125, 555, 44];
+// console.log(bills);
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+// console.log(tips);
+// console.log(tips.indexOf(18.75), tips.indexOf(100));
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[bills.length - 1] + tips[tips.length - 1]];
+// console.log(total);
+
+// const totalNew = [bills[0] + tips[0], bills[1] + tips[1]];
+// console.log(totalNew);
+// Why Upper method is not working?
+
+function calcTip(bill) {
+  const tip = bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill; //ternary operator (alternate to if/else - smaller equations! )
+  return tip;
+}
+const bills = [125, 555, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[bills.length - 1] + tips[tips.length - 1]];
+console.log(total);
+*/
+
+/*============================ introduction to Object =================================*/
